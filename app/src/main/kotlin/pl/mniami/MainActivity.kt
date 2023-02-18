bea490ea164c0581c7ef1308170f30df2f020d30
Dawid <dawid_fendler98@wp.pl>
2023-02-18 23:26:01 +0100

    task-02: Add splash screen api

--- app/src/main/kotlin/pl/mniami/MainActivity.kt
+++ app/src/main/kotlin/pl/mniami/MainActivity.kt
@@ -2,10 +2,12 @@ package pl.mniami
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
     }
 }
