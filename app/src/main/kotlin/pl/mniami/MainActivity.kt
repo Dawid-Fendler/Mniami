08f03c4f1409f8f6e48abf8415a8df3ceec25d9c
Dawid <dawid_fendler98@wp.pl>
2023-02-18 23:14:04 +0100

    task-01: Setup project

---
+++ app/src/main/kotlin/pl/mniami/MainActivity.kt
@@ -0,0 +1,11 @@
+package pl.mniami
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
