dc6bfd44600bc47226535cd9f02498b6cd4939ef
dawid.fendler <dawid_fendler98@wp.pl>
2023-03-26 13:16:02 +0200

    task-20: prepare recipe details view

---
+++ common/ui/src/main/kotlin/pl/ui/TextViewExtensions.kt
@@ -0,0 +1,8 @@
+package pl.ui
+
+import android.widget.TextView
+import androidx.core.text.HtmlCompat
+
+fun TextView.parseHtml(text: String){
+    this.text = HtmlCompat.fromHtml(text, HtmlCompat.FROM_HTML_MODE_COMPACT)
+}
--- presentation/recipedetails/src/main/kotlin/pl/recipedetails/RecipeDetailsFragment.kt
+++ presentation/recipedetails/src/main/kotlin/pl/recipedetails/RecipeDetailsFragment.kt
@@ -1,10 +1,99 @@
 package pl.recipedetails
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import coil.load
 import dagger.hilt.android.AndroidEntryPoint
 import pl.architecture.base.BaseFragment
+import pl.domain.model.recipedetails.RecipeDetailsUiModel
 import pl.presentation.recipedetails.R
 import pl.presentation.recipedetails.databinding.FragmentRecipeDetailsBinding
+import pl.recipedetails.RecipeDetailsViewState.Loading
+import pl.recipedetails.RecipeDetailsViewState.RecipeDetailsLoadFailure
+import pl.recipedetails.RecipeDetailsViewState.RecipeDetailsLoaded
+import pl.ui.parseHtml
 
 @AndroidEntryPoint
 class RecipeDetailsFragment :
-    BaseFragment<FragmentRecipeDetailsBinding>(R.layout.fragment_recipe_details)
+    BaseFragment<FragmentRecipeDetailsBinding>(R.layout.fragment_recipe_details) {
+
+    private val viewModel: RecipeDetailsViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setListeners()
+
+        observeRecipeDetails(1)
+    }
+
+    private fun setListeners() {
+
+    }
+
+    private fun observeRecipeDetails(id: Int) {
+        viewModel.onStart(id)
+        viewModel.recipeDetailsViewState.observe(viewLifecycleOwner) { result ->
+            when (result) {
+                is RecipeDetailsLoaded -> showRecipeDetailsLoadedState(result.result)
+                is RecipeDetailsLoadFailure -> showRecipeDetailsLoadFailureState()
+                is Loading -> showLoadingState()
+            }
+        }
+    }
+
+    private fun showRecipeDetailsLoadedState(result: RecipeDetailsUiModel) {
+        initView(result)
+    }
+
+    private fun initView(result: RecipeDetailsUiModel) {
+        initRecipeImage(result.image)
+        initRecipeTitle(result.title)
+        initRecipeSummary(result.summary)
+        initDietTypes(result.dietTypes)
+        initDishTypes(result.dishTypes)
+    }
+
+    private fun initRecipeImage(imageUrl: String) {
+        binding.recipeImageView.load(imageUrl) {
+            crossfade(true)
+        }
+    }
+
+    private fun initRecipeTitle(title: String) {
+        binding.recipeTitleText.text = title
+    }
+
+    private fun initRecipeSummary(summary: String) {
+        binding.summaryText.parseHtml(summary)
+    }
+
+    private fun initDietTypes(dietTypes: List<String>) {
+        dietTypes.forEach { dietTypeName ->
+            binding.dietTypeContainer.addView(
+                ChipView(requireContext()).apply {
+                    chipText = dietTypeName
+                }
+            )
+        }
+    }
+
+    private fun initDishTypes(dishTypes: List<String>) {
+        dishTypes.forEach { dishTypeName ->
+            binding.mealTypeContainer.addView(
+                ChipView(requireContext()).apply {
+                    chipText = dishTypeName
+                }
+            )
+        }
+    }
+
+    private fun showRecipeDetailsLoadFailureState() {
+
+    }
+
+    private fun showLoadingState() {
+
+    }
+}
